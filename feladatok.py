f633475b37dde7817421aa0a66d532357f642e13
Tar Gergő (SZF_2023_2025) <tar.gergo@diak.szamalk-szalezi.hu>
2023-10-26 11:09:13 +0200

    4.fel pontosítása

--- feladatok.py
+++ feladatok.py
@@ -38,13 +38,16 @@ def fel4(szam):
     if szam < 0:
         print("Hiba:pozitivnek kell ,hogy legyen !")
     while i <= szam:
-        if i % 3 == 0 and i == szam:
+        if i % 6 == 0 and i == szam:
+            print("BUMMBAM", end="")
+        elif i % 6 == 0:
+            print("BUMMBAM", end=",")
+        elif i == szam and i % 3 == 0:
             print("BOOM", end="")
         elif i % 3 == 0:
             print("BOOM", end=",")
-        elif i == szam and i % 3 != 0:
-            print(i, end="")
+        elif i == szam and i % 2 == 0:
+            print("BAM", end="")
         else:
             print(i, end=",")
         i += 1
-
